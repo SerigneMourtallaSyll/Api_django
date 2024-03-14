e863f8a9d1edf92d0854a9793b43a504d0d5b9b4
Mourtallasyll <serignemourtallasyll86@gmail.com>
2024-03-14 09:40:15 +0000

    Modification de la lecture de l'image

--- controllerEmail/urls.py
+++ controllerEmail/urls.py
@@ -7,10 +7,11 @@
 # ]
 
 from django.urls import path , include
-from .views import SendTemplateMailView , render_image, GetEmailTrackingData
+from .views import SendTemplateMailView , render_image, GetEmailTrackingData, TrackingPixelView
 
 urlpatterns = [
     path('send/render_image/', render_image.as_view(), name='render_image'),
     path('send/', SendTemplateMailView.as_view(), name='send_template'),
     path('get_image/', GetEmailTrackingData.as_view(), name='get_template'),
+    path('tracking_pixel/', TrackingPixelView.as_view(), name='tracking_pixel'),
 ]
--- controllerEmail/views.py
+++ controllerEmail/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from rest_framework import status
 from .models import EmailTracker, EmailTracking
 from django.core.mail import EmailMultiAlternatives
@@ -20,8 +20,9 @@ class SendTemplateMailView(APIView):
         target_user_email = request.data.get('email')
         mail_template = get_template("index.html") 
         context_data_is = dict()
-        # Construire l'URL de l'image de suivi
-        image_url = request.build_absolute_uri(reverse("render_image"))
+        # Construire l'URL de l'image de suivi avec un paramètre de requête aléatoire
+        random_param = f"random={random.randint(1, 100000)}"
+        image_url = request.build_absolute_uri(reverse("TrackingPixelView")) + '?' + random_param
         context_data_is["image_url"] = image_url
         email_tracker = EmailTracker.objects.create(
             recipient_email=target_user_email,
@@ -49,7 +50,6 @@ class render_image(APIView):
             response = HttpResponse(content_type=f"image/{image_format.lower()}")
             image.save(response, image_format)
             
-            # Enregistrer l'ouverture de l'e-mail dans la base de données
             email_id = request.query_params.get('email_id')
             if email_id:
                 try:
@@ -65,6 +65,28 @@ class render_image(APIView):
             image_url = request.build_absolute_uri(reverse("render_image")) + '?' + random_param
             
             return response
+
+
+class TrackingPixelView(APIView):
+    def get(self, request):
+        # Créez une réponse HTTP vide avec un contenu d'un seul pixel transparent
+        response = HttpResponse(content_type='image/gif')
+        response['Content-Disposition'] = 'inline'
+        response.write(b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x4c\x01\x00;')
+        
+        # Enregistrez l'ouverture de l'e-mail dans la base de données
+        email_id = request.GET.get('email_id')
+        if email_id:
+            try:
+                email_tracker = EmailTracker.objects.get(id=email_id)
+                email_tracking = EmailTracking.objects.create(email=email_tracker)
+                email_tracking.opened_at = datetime.now()
+                email_tracking.save()
+            except EmailTracker.DoesNotExist:
+                pass
+
+        return response
+
 
 class GetEmailTrackingData(APIView):
     def get(self, request):
