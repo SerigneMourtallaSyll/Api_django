d79b79ed8959c7f0c23271884dd824911f7b91a4
Mourtallasyll <serignemourtallasyll86@gmail.com>
2024-03-14 09:57:10 +0000

    Modification de la lecture de l'image

--- controllerEmail/urls.py
+++ controllerEmail/urls.py
@@ -7,11 +7,11 @@
 # ]
 
 from django.urls import path , include
-from .views import SendTemplateMailView , render_image, GetEmailTrackingData, TrackingPixelView
+from .views import SendTemplateMailView , render_image, GetEmailTrackingData, tracking_pixel
 
 urlpatterns = [
     path('send/render_image/', render_image.as_view(), name='render_image'),
     path('send/', SendTemplateMailView.as_view(), name='send_template'),
     path('get_image/', GetEmailTrackingData.as_view(), name='get_template'),
-    path('tracking/', TrackingPixelView.as_view(), name='tracking_pixel'),
+    path('tracking/', tracking_pixel.as_view(), name='tracking_pixel'),
 ]
