c42a9b5fba616906c11d1a1a0bc9e882efdd1640
Mourtallasyll <serignemourtallasyll86@gmail.com>
2024-03-25 16:10:18 +0000

    Modification de la logique de l'envoi de l'email

--- controllerEmail/views.py
+++ controllerEmail/views.py
@@ -54,11 +54,11 @@ class SendTemplateMailView(APIView):
             html_detail = mail_template.render(context_data_is)
             msg = EmailMultiAlternatives(objet, html_detail, 'serignemourtallasyll86@gmail.com', [email])
             msg.content_subtype = 'html'
-
             if document:
                 msg.attach_file(email_tracker.document.path)
-
             msg.send()
+
+        return Response({"success": True})
 
 
     def generate_tracking_pixel_url(self, request, email_id):
